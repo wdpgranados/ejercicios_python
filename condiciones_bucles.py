0244c23a0a05c01f6b356d787fcd93c6b99a63b7
william <wdpgrandos@gmail.com>
2026-03-02 22:50:32 -0500

    practicando while y for

--- condiciones_bucles.py
+++ condiciones_bucles.py
@@ -1,3 +1,4 @@
+
 # las condiciones sirven para tomar decisiones en en código
 edad = 18
 if edad >=18:
@@ -44,4 +45,44 @@ elif temperatura_actual < temperatura_optima - 2:
 else:
     print("la temperatura es agradable, no es necesario ajustar el termostato")
         
+import math
+numero = int(input("ingrese un numero positivo:"))
+while numero < 0:
+    print("no puedes ingresar numeros negativos")
+    numero = int(input("ingrese un numero positivo:"))
+print(f"la raiz cuadrada del {numero} es {math.sqrt(numero):.2f}")
+
+
+# el bucle while se ejecuta mientras la condición sea verdadera y se detiene cuando la condición se vuelve falsa
+i = -1
+while i >= -10:
+    print(i)
+    i -= 1
     
+i = 1
+while i <= 10:
+    if i == 9:
+        break # romple el bucle cuando i es igual a 9
+    print(i)
+    i +=1
+
+numero = 1
+while numero <= 10:
+    if numero  % 3 == 0:
+        numero +=1
+        continue # salta el resto del código en la iteración actual y pasa a la siguiente iteración
+    print(numero)
+    numero +=1   # incrementa el numero en cada iteración para evitar un bucle infinito
+        
+            
+    
+    
+# bugle for se utiliza para iterar sobre una secuencia de elementos, como listas, tuplas o rangos de números
+lista = ["manzana", "banana", "naranja","pera"]
+
+for fruta in lista:
+    print(fruta)    
+
+for i in range(1,6):
+    print(i)
+        
